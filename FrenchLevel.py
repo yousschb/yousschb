bc1e07854706f2e04a6c0d7d3e30e6cdde6514bb
yousschb <149575962+yousschb@users.noreply.github.com>
2023-12-20 06:31:15 +0100

    Update FrenchLevel.py

--- FrenchLevel.py
+++ FrenchLevel.py
@@ -1,78 +1,40 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import numpy as np
+import types
+from transformers import FlaubertTokenizer, TFFlaubertForSequenceClassification
 import tensorflow as tf
-from sklearn.preprocessing import LabelEncoder
-from transformers import FlaubertTokenizer, TFFlaubertModel, TFFlaubertForSequenceClassification
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from sklearn.model_selection import train_test_split
+import numpy as np
 
-# Charger les données
-train_data = pd.read_csv('training_data.csv')
+# Custom hash function to bypass hashing of the load_model function
+def bypass_hashing(func):
+    return 0
 
-X = train_data['sentence']
-y = train_data['difficulty']
+# Function to load the FlauBERT model
+# Function to load the trained FlauBERT model
+@st.cache(allow_output_mutation=True, hash_funcs={types.FunctionType: bypass_hashing})
+def load_model():
+    model_load_path = "/Users/mac/Desktop/my_flauBERT_model" 
+    model = TFFlaubertForSequenceClassification.from_pretrained(model_load_path, num_labels=6)
+    return model
+    
+# Function to encode text for FlauBERT
+def encode_text(text, tokenizer, max_length=128):
+    encoded_dict = tokenizer.encode_plus(
+        text,
+        add_special_tokens=True,
+        max_length=max_length,
+        pad_to_max_length=True,
+        return_attention_mask=True,
+        return_tensors='tf',
+    )
+    return encoded_dict['input_ids'], encoded_dict['attention_mask']
 
-# Encodage des labels
-label_encoder = LabelEncoder()
-y_encoded = label_encoder.fit_transform(y)
-
-# Diviser les données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
-
-# Initialiser le tokenizer FlauBERT
+# Load FlauBERT tokenizer
 tokenizer = FlaubertTokenizer.from_pretrained('flaubert/flaubert_base_cased')
 
-# Ppar les données pour FlauBERT
-def encode_for_flaubert(sentences, max_length=128):
-    input_ids = []
-    attention_masks = []
+# Load the model
+model = load_model()
 
-    for sentence in sentences:
-        encoded_dict = tokenizer.encode_plus(
-            sentence,
-            add_special_tokens=True,
-            max_length=max_length,
-            pad_to_max_length=True,
-            turn_attention_mask=True,
-            turn_tensors='tf',
-        )
-        input_ids.append(encoded_dict['input_ids'])
-        attention_masks.append(encoded_dict['attention_mask'])
-
-    input_ids = tf.concat(input_ids, 0)
-    attention_masks = tf.concat(attention_masks, 0)
-
-    return input_ids, attention_masks
-
-train_input_ids, train_attention_masks = encode_for_flaubert(X_train, max_length=128)
-test_input_ids, test_attention_masks = encode_for_flaubert(X_test, max_length=128)
-
-
-# Charger le modèle FlauBERT p-entraîné pour la classification de séquence
-model = TFFlaubertForSequenceClassification.from_pretrained('flaubert/flaubert_base_cased', num_labels=len(label_encoder.classes_), from_pt=True)
-
-# Compiler le modèle
-optimizer = Adam(learning_rate=5e-5)
-loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
-model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
-
-# Entraîner le modèle
-model.fit(
-    [train_input_ids, train_attention_masks],
-    y_train,
-    epochs=1,
-    batch_size=32,
-    validation_data=([test_input_ids, test_attention_masks], y_test)
-)
-
-
-
-
-# Stamlit interface
+# Streamlit interface
 st.title('French Text Difficulty Predictor')
 user_input = st.text_area("Enter a sentence in French", "")
 
