1861f47d71df5184e9a3e8830f6079598a09ab5a
yousschb <149575962+yousschb@users.noreply.github.com>
2023-12-20 08:05:50 +0100

    Update FrenchLevel.py

--- FrenchLevel.py
+++ FrenchLevel.py
@@ -120,7 +120,13 @@ elif option == 'Jeu de Prédiction de Niveau':
         st.subheader(f"Votre score : {st.session_state['score']} / 10")
         for phrase, user_guess, predicted_level, correct in st.session_state['game_history']:
             color = "green" if correct else "red"
-            st.markdown(f"**{phrase}** - <span style='color: {color}'>Votre réponse : {user_guess}, Niveau prédit
+            st.markdown(f"**{phrase}** - <span style='color: {color}'>Votre réponse : {user_guess}, Niveau prédit : {predicted_level}</span>", unsafe_allow_html=True)
 
-
+        if st.button("Recommencer le Jeu"):
+            st.session_state['score'] = 0
+            st.session_state['phrase_count'] = 0
+            st.session_state['current_phrase'] = None
             st.session_state['game_history'] = []
+
+
+
